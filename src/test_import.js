4c6b5ff49af311f968bec96ab3196379c0d00aeb
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-03-11 18:20:25 -0500

    adds test_import file

---
+++ src/test_import.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Test_import extends Component {
+    render() {
+        return(
+            <div>
+                testing Test_import
+            </div>
+        )
+    }
+}
+
+export default Test_import;
