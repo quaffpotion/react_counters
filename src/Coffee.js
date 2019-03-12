c1878b92ac43cd2abd88b010658f72c5eef7c9ac
quaffpotion <43005901+quaffpotion@users.noreply.github.com>
2019-03-12 16:34:10 -0500

    difference between anonymous functions and binding

--- src/Coffee.js
+++ src/Coffee.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 
 class Coffee extends Component {
+
+    //This is included to give a contrast to using arrow functions.
+    //See the different versions of increment and decrement below
+    constructor() {
+        //console.log(this) will throw an error because `this` isn't initialized
+        //until super is called. However in reactarmoury tutorial they don't seem
+        //to need to do this (in Timer.js)
+        super()
+
+        //Coffee object with state.cups = 0 i.e. constructor runs after other object
+        //variables are initialized
+        console.log(this)
+        this.increment = this.increment.bind(this)
+
+    }
+
     state = {
         cups: 0
     }
@@ -13,7 +29,9 @@ class Coffee extends Component {
         })
     }
 
-    increment = () => {
+    //note: not an arrow function so we need to use the code in the `constructor` function
+    //to force increment to always be called by the coffee object so it's `this` is correct.
+    increment() {
         this.setState( (prevState) => {
             return {
                 cups: prevState.cups + this.props.step
